5867d618180293f0aef7691dd857c205243468e5
brfialho <brfialho@student.42sp.org.br>
2025-10-22 14:37:50 -0300

    added matrix functions in lib

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 16:28:38 by brfialho          #+#    #+#             */
-/*   Updated: 2025/10/21 22:49:54 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/10/22 14:27:55 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,13 @@ int	main(int argc, char **argv)
 	if (fd < 0)
 		return (ft_printf("Error : %s\n", strerror(errno)));
 	
-	char *lines[6];
-	int i = 0;
-	while ((lines[i] = get_next_line(fd)))
-		ft_printf("LINE: %s\n", lines[i++]);
+	t_tab	map;
+	if (!ft_tab_innit_empty(&map, 5, 15, sizeof(char)))
+		return (1);
+	int i = -1;
+	while ((map.tab[++i] = get_next_line(fd)))
+		;
+	for (int j = 0; j < i; j++)
+		ft_printf("LINE: %d LEN: %s", ft_strlen(map.tab[j]), map.tab[j]);
+	ft_tab_free_content(&map);
 }
