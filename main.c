db6cc044976d3a9402ec7f4acecd55f09b90edd7
brfialho <brfialho@student.42sp.org.br>
2025-10-21 23:29:46 -0300

    testing

--- main.c
+++ main.c
@@ -6,30 +6,24 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 16:28:38 by brfialho          #+#    #+#             */
-/*   Updated: 2025/10/21 21:57:39 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/10/21 22:49:54 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-// int	main(void)
-// {
-// 	void	*mlx = mlx_init();
-// 	void	*mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-// 	mlx_loop(mlx);
-
-// 	free(mlx_win);
-// 	free(mlx);
-// }
-
 int	main(int argc, char **argv)
 {
+	if (argc != 2)
+		return (ft_printf("Error\n"));
 
-	int fd = open("file.txt", O_RDONLY);
-	// perror("Something went wrong");
-	printf("ERROR : %s\n", strerror(errno));
+	int fd = open(argv[1], O_RDONLY);
 
-	(void)fd;
-	(void)argc;
-	(void)argv;
+	if (fd < 0)
+		return (ft_printf("Error : %s\n", strerror(errno)));
+	
+	char *lines[6];
+	int i = 0;
+	while ((lines[i] = get_next_line(fd)))
+		ft_printf("LINE: %s\n", lines[i++]);
 }
