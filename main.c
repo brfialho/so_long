8fd1e1444b504bcf16544b879e7aa9f99b45c662
brfialho <brfialho@student.42sp.org.br>
2025-10-21 22:25:00 -0300

    added all libraries necessary

--- main.c
+++ main.c
@@ -6,18 +6,30 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 16:28:38 by brfialho          #+#    #+#             */
-/*   Updated: 2025/10/18 19:16:37 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/10/21 21:57:39 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-int	main(void)
-{
-	void	*mlx = mlx_init();
-	void	*mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
+// int	main(void)
+// {
+// 	void	*mlx = mlx_init();
+// 	void	*mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+// 	mlx_loop(mlx);
 
-	free(mlx_win);
-	free(mlx);
+// 	free(mlx_win);
+// 	free(mlx);
+// }
+
+int	main(int argc, char **argv)
+{
+
+	int fd = open("file.txt", O_RDONLY);
+	// perror("Something went wrong");
+	printf("ERROR : %s\n", strerror(errno));
+
+	(void)fd;
+	(void)argc;
+	(void)argv;
 }
--- main.h
+++ main.h
@@ -6,14 +6,19 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 19:12:58 by brfialho          #+#    #+#             */
-/*   Updated: 2025/10/21 20:45:24 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/10/21 22:03:32 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
  #define MAIN_H
 
-#include "libft.h"
-#include "mlx.h"
+# include "libft.h"
+# include "mlx.h"
+# include <stdio.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <string.h>
+# include <sys/time.h>
 
 #endif
