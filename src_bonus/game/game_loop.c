126aac1a19223885a5fe0e9e7dcc3e56425e5da3
brfialho <brfialho@student.42sp.org.br>
2025-11-04 17:57:37 -0300

    ramdom function done

--- src_bonus/game/game_loop.c
+++ src_bonus/game/game_loop.c
@@ -12,10 +12,36 @@
 
 #include "main_bonus.h"
 
+// int	get_ramdom_pos(void)
+// {
+// 	int	min;
+// 	int	max;
+// 	int ramdom;
+
+// 	min = 1;
+// 	max = 4;
+
+// 	ramdom = (rand() % (max - min + 1)) + min;
+// 	return (ramdom);
+// }
+
+// void move_monsters(t_game *game)
+// {
+// 	game = 0;
+// 	get_ramdom_pos();
+// }
+
+
+// void	handle_monsters(t_game *game)
+// {
+// 	move_monsters(game);
+// }
+
 int	game_loop(t_game *game)
 {
 	handle_keys(game);
-	all_key_release (game);
+	all_key_release(game);
+	// handle_monsters(game);
 	render_image(game);
 	ft_usleep(10000);
 	return (0);
