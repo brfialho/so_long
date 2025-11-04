b052a989bdab17af89c8fcf16573b8b96fc1bf15
brfialho <brfialho@student.42sp.org.br>
2025-11-04 20:22:22 -0300

    monster movement done

--- src_bonus/game/game_loop.c
+++ src_bonus/game/game_loop.c
@@ -12,59 +12,68 @@
 
 #include "main_bonus.h"
 
-// t_pos	get_ramdom_move(t_pos next)
-// {
-// 	int	min;
-// 	int	max;
-// 	int ramdom;
+t_pos	get_ramdom_move(t_pos next)
+{
+	int	min;
+	int	max;
+	int ramdom;
 
-// 	min = 1;
-// 	max = 4;
+	min = 1;
+	max = 4;
 
-// 	ramdom = (rand() % (max - min + 1)) + min;
-// 	if (ramdom == 1)
-// 		return ((t_pos){next.row - 1, next.col});
-// 	else if (ramdom == 2)
-// 		return ((t_pos){next.row, next.col});
-// 	else if (ramdom == 3)
-// 		return ((t_pos){next.row, next.col});
-// 	else
-// 		return ((t_pos){next.row, next.col});
-// }
+	ramdom = (rand() % (max - min + 1)) + min;
+	if (ramdom == 1)
+		return ((t_pos){next.row - 1, next.col});
+	else if (ramdom == 2)
+		return ((t_pos){next.row, next.col + 1});
+	else if (ramdom == 3)
+		return ((t_pos){next.row + 1, next.col});
+	else
+		return ((t_pos){next.row, next.col - 1});
+}
 
 
-// void	move_monster(t_game *game, t_pos next_pos)
-// {
-// 	char		next_tile;
+void	move_monster(t_game *game, t_pos next_pos, int i)
+{
+	char		next_tile;
+	int			is_quest;
 
-// 	next_tile = ((char **)game->map.tab)[next_pos.row][next_pos.col];
-// 	if (next_tile == WALL)
-// 		return ;
-// 	((char **)game->map.tab)[next_pos.row][next_pos.col] = PLAYER;
-// 	((char **)game->map.tab)[game->player.row][game->player.col] = FLOOR;
-// 	if (game->player.row == game->exit.row
-// 		&& game->player.col == game->exit.col)
-// 		((char **)game->map.tab)[game->player.row][game->player.col] = EXIT;
-// 	game->player = next_pos;
-// }
+	is_quest = FALSE;
+	next_tile = ((char **)game->map.tab)[next_pos.row][next_pos.col];
+	if (next_tile == WALL || next_tile == ENEMY)
+		return ;
+	if (next_tile == QUEST)
+		is_quest = TRUE;
+	if (next_tile == PLAYER && ft_printf ("\n\n ### GAME OVER ###\n\n"))
+		destroy_game(game);
+	((char **)game->map.tab)[next_pos.row][next_pos.col] = ENEMY;
+	((char **)game->map.tab)[game->monster[i].pos.row][game->monster[i].pos.col] = FLOOR;
+	if (game->monster[i].pos.row == game->exit.row
+		&& game->monster[i].pos.col == game->exit.col)
+		((char **)game->map.tab)[game->monster[i].pos.row][game->monster[i].pos.col] = EXIT;
+	if (is_quest)
+		((char **)game->map.tab)[game->monster[i].pos.row][game->monster[i].pos.col] = QUEST;
+	game->monster[i].pos = next_pos;
+}
 
-// void	handle_monsters(t_game *game)
-// {
-// 	int			i;
-// 	t_direction	d;
+void	handle_monsters(t_game *game)
+{
+	static int	cooldown = 39;
+	int			i;
 
-// 	i = -1;
-// 	while (++i < game->obj.x_count)
-// 		{
-			
-// 		}
-// }
+	if (cooldown++ < 40)
+		return ;
+	cooldown = 0;
+	i = -1;
+	while (++i < (int)game->obj.x_count)
+		move_monster(game, get_ramdom_move(game->monster[i].pos), i);
+}
 
 int	game_loop(t_game *game)
 {
 	handle_keys(game);
 	all_key_release(game);
-	// handle_monsters(game);
+	handle_monsters(game);
 	render_image(game);
 	ft_usleep(10000);
 	return (0);
