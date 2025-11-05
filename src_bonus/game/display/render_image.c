21de3caac04741f5253057f8fc0259b6dd701864
brfialho <brfialho@student.42sp.org.br>
2025-11-05 16:25:30 -0300

    progressing in render image

--- src_bonus/game/display/render_image.c
+++ src_bonus/game/display/render_image.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 18:42:39 by brfialho          #+#    #+#             */
-/*   Updated: 2025/11/05 16:09:35 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/11/05 16:24:26 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	render_image(t_game *game)
 		col = -1;
 		while (++col < (int)game->map.cols)
 		{
+			if (((char **)game->map.tab)[row][col] == ENEMY)
 			mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win_ptr, game->mlx.img[0], col * SQUARE, row * SQUARE);
 		}
 	}
