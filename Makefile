5e20dba56f50c808a0dd2dad0d355deea3cc03e5
brfialho <brfialho@student.42sp.org.br>
2025-11-06 17:24:57 -0300

    unified destroys

--- Makefile
+++ Makefile
@@ -55,9 +55,7 @@ BONUS_SRC = src_bonus/main.c \
 		src_bonus/validation/map_validator.c \
 		src_bonus/game/display/display_moves.c \
 		src_bonus/game/display/render_image.c \
-		src_bonus/game/init_destroy/destroy_game.c \
-		src_bonus/game/init_destroy/destroy_img.c \
-		src_bonus/game/init_destroy/destroy_mlx.c \
+		src_bonus/game/init_destroy/destroy.c \
 		src_bonus/game/init_destroy/init_game.c \
 		src_bonus/game/init_destroy/init_mlx_display.c \
 		src_bonus/game/init_destroy/init_img.c \
--- src_bonus/game/init_destroy/destroy.c
+++ src_bonus/game/init_destroy/destroy.c
@@ -1,16 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   destroy_mlx.c                                      :+:      :+:    :+:   */
+/*   destroy.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/30 17:14:54 by brfialho          #+#    #+#             */
-/*   Updated: 2025/11/05 18:50:27 by brfialho         ###   ########.fr       */
+/*   Created: 2025/11/03 18:26:21 by brfialho          #+#    #+#             */
+/*   Updated: 2025/11/06 17:23:25 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main_bonus.h"
+
+int	destroy_game(t_game *game)
+{
+	destroy_img(&game->mlx);
+	destroy_mlx(&game->mlx);
+	if (game->monster)
+		free(game->monster);
+	ft_tab_free_content(&game->map);
+	ft_printf("\nFinal Score: %d\nThanks for Playing !!\n", (int)game->moves);
+	exit(0);
+}
 
 void	destroy_mlx(t_mlx *mlx)
 {
@@ -21,4 +32,14 @@ void	destroy_mlx(t_mlx *mlx)
 		mlx_destroy_display(mlx->mlx_ptr);
 		free(mlx->mlx_ptr);
 	}
+}
+
+void	destroy_img(t_mlx *mlx)
+{
+	int	i;
+
+	i = -1;
+	while (++i < TEXTURES)
+		if (mlx->img[i])
+			mlx_destroy_image(mlx->mlx_ptr, mlx->img[i]);
 }
